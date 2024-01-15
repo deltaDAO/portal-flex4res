5d4b47a3d8d704319ec9e5daaebbc66887e801a6
Moritz Kirstein <mkirstein@protonmail.com>
2024-01-15 14:10:53 +0100

    style: initial design adjustments

--- src/components/@shared/atoms/Logo/index.tsx
+++ src/components/@shared/atoms/Logo/index.tsx
@@ -3,5 +3,12 @@ import LogoAsset from '@images/gaia-x-logo.svg'
 import styles from './index.module.css'
 
 export default function Logo(): ReactElement {
-  return <LogoAsset className={styles.logo} />
+  return (
+    <img
+      className={styles.logo}
+      alt={`Logo`}
+      src={`/images/ecosystem/flex4res-logo.webp`}
+    />
+  )
+  // <LogoAsset className={styles.logo} />
 }
