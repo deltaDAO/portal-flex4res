6e53587b8ad6ac70a3f4c5dd33de70aa2822c426
Moritz Kirstein <mkirstein@protonmail.com>
2024-01-25 16:22:21 +0100

    feat: update logo

--- src/components/@shared/atoms/Logo/index.tsx
+++ src/components/@shared/atoms/Logo/index.tsx
@@ -1,14 +1,7 @@
 import { ReactElement } from 'react'
-import LogoAsset from '@images/gaia-x-logo.svg'
+import LogoAsset from '@images/flex4res-logo.svg'
 import styles from './index.module.css'
 
 export default function Logo(): ReactElement {
-  return (
-    <img
-      className={styles.logo}
-      alt={`Logo`}
-      src={`/images/ecosystem/flex4res-logo.webp`}
-    />
-  )
-  // <LogoAsset className={styles.logo} />
+  return <LogoAsset className={styles.logo} />
 }
